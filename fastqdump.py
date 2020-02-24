541cf01aa9f33cdf7fba82e7dac8b6d77565c872
Kathleen M. Delany <katiemdelany@gmail.com>
2020-02-24 11:33:04 -0600

    tested fastq dump portion of program

--- fastqdump.py
+++ fastqdump.py
@@ -10,18 +10,21 @@ os.getcwd()
 def InptFiles(SRR):
     """
     This will retrieve the transcriptomes of given SRR numbers and convert them to
-    paired-end fastq files. 
+    paired-end fastq files. Files from wget download as SRR#######.1 get renamed to SRR#######  
 
     """
     #getFiles = 'wget https://sra-download.st-va.ncbi.nlm.nih.gov/sos1/sra-pub-run-12/SRR' + SRR + '/' + SRR + '.1'
     #wget https://sra-download.st-va.ncbi.nlm.nih.gov/sos1/sra-pub-run/12/SRR5660030/SRR5660030.1
-    renameFile = 
+    renameFile = 'mv '+ str(SRR)+'.1 ' + SRR
     splitFiles = 'fastq-dump -I --split-files '+ str(SRR)
     #os.system(getFiles)
+    os.system(renameFile)
     os.system(splitFiles)
 
 
 def main():
+    """ Takes in SRR id number arguments in command line and runs through"""
+    
     parser = argparse.ArgumentParser(description= 'Process SRR (RunID) numbers')
     parser.add_argument('SRR', metavar= 'N', type=str, nargs = '+', help = 'SRRs')
     parser.add_argument('--downloadfiles', help='Retrieve SRA from database and convert to paired-end fastq files')
