d7b582fd79d7f207a346ea175f2106b32b32d4dd
Kathleen M. Delany <katiemdelany@gmail.com>
2020-02-25 19:57:06 -0600

    Updates

--- miniMain.py
+++ miniMain.py
@@ -28,21 +28,13 @@ def InptFiles(SRR):
     paired-end fastq files. Files from wget download as SRR#######.1 get renamed to SRR#######  
 
     """
-    #getFiles = 'wget https://sra-download.st-va.ncbi.nlm.nih.gov/sos1/sra-pub-run-12/SRR' + SRR + '/' + SRR + '.1'
-    #wget https://sra-download.st-va.ncbi.nlm.nih.gov/sos1/sra-pub-run/12/SRR5660030/SRR5660030.1
+    getFiles = 'wget https://sra-download.st-va.ncbi.nlm.nih.gov/sos1/sra-pub-run-12/SRR' + SRR + '/' + SRR + '.1'
     renameFile = 'mv '+ str(SRR)+'.1 ' + SRR
     splitFiles = 'fastq-dump -I --split-files '+ str(SRR)
-    #os.system(getFiles)
+    os.system(getFiles)
     os.system(renameFile)
     os.system(splitFiles)
 
-#    rename_file_cmd = shlex.split('mv {}.1 {}'.format(SRR, SRR))
-#    with open('mv_cmd_log.txt', 'ab+') as mv_out:
-#        subprocess.run(rename_file_cmd, stdout=mv_out, stderr=mv_out)
-
-#    split_files_cmd = shlex.split('fastq-dump -I --split-files {}'.format(SRR))
-#    with open('fastq_dump_output.txt', 'ab+') as fastq_dump_out:
-#        subprocess.run(split_files_cmd, stdout=fastq_dump_out, stderr=fastq_dump_out)
 
 def getTranscriptomeIndex():
     outFasta = open("EF999921.fasta", "w")
@@ -107,7 +99,7 @@ def bowtie2build(SRR):
     """ Builds a Bowtie index for HCMV """
     build_cmd = 'bowtie2-build ./EF999921.fasta HCMV'
     os.system(build_cmd)
-    bowtie_cmd = 'bowtie2 --quiet --al-conc --no-unal -x HCMV -1 '+SRR+'_1.fastq -2'+SRR+'_2.fastq -S '+SRR+ '.sam'
+    bowtie_cmd = 'bowtie2 --quiet --no-unal -x --al-conc HCMV -1 '+SRR+'_1.fastq -2'+SRR+'_2.fastq -S '+SRR+ '.sam'
     os.system(bowtie_cmd)
 
 
@@ -228,49 +220,42 @@ def blast():
 def main():
     """ Takes in SRR id number arguments in command line and runs through"""
     
-    # parser = argparse.ArgumentParser(description= 'Process SRR (RunID) numbers')
-    # parser.add_argument('SRR', metavar= 'N', type=str, nargs = '+', help = 'SRRs')
     args = arg_parser()
     
     logging.info('SRA values: %s', args.srrfiles)
-
+    
     print(args.srrfiles)
-    
-#    for i in args.SRR:
+  
+    #This is where I download and split the SRR data files. Commented out to save time.
+#    for i in args.srrfiles:
 #       InptFiles(i)
-#    with open('miniproject.log', 'a') as f_out:
-#        f_out.write('SRA files download done.')
-#        f_out.close()
-#    result =  getTranscriptomeIndex()
-#    logging.info('The HCMV genome (EF999921) has '+str(result))
+
+    result =  getTranscriptomeIndex()
+    logging.info('The HCMV genome (EF999921) has '+str(result))
 
 
     
 
-#    for i in args.SRR:
-#        Kallisto(i)
+    for i in args.srrfiles:
+        Kallisto(i)
 
-#    SleuthInput()
-##still have to run R code in command line
+    SleuthInput()
+    Sleuth()
+
     
-    SRRs = []
-#    for i in args.SRR:
-       # bowtie2build(i)
-#        Sam2Fastq(i)
-#        getNumReads(i)
-#        SRRs.append(i)
-#    SPAdes(SRRs)
+    for i in args.srrfiles:
+        bowtie2build(i)
+        Sam2Fastq(i)
+        getNumReads(i)
+        
+    SPAdes(args.srrfiles)
 
-#    numContigs()
+    numContigs()
     countContigs()
     assembleContigs()
     blast()  
-
-
-
-
-
     
+
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logFormatter = '%(message)s'
